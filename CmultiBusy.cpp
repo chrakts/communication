8fe52fccf889f2332a82510a6e65c21f83334335
Christof <chrak@cmja.de>
2020-02-23 07:18:52 +0100

    Interrupt-Prios geändert, deInint-Funktion hinzu

--- CmultiBusy.cpp
+++ CmultiBusy.cpp
@@ -8,6 +8,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <uartHardware.h>
+//#include "../loraSupervisor/myConstants.h"
 
 extern volatile uint8_t sendFree;
 extern volatile uint8_t sendAnswerFree;
@@ -16,27 +17,34 @@ extern volatile uint8_t sendAnswerFree;
 
 void initReadMonitor()
 {
-	Busy_Control_Port.INTCTRL  = PORT_INT0LVL0_bm ; // Low-Level interrupt 0 for PORTD
-	Busy_Control_Port.INT0MASK = Busy_Control_Pin;
-	Busy_Control_Port.Busy_Control_PinCtrl = PORT_ISC_BOTHEDGES_gc | PORT_OPC_PULLUP_gc ;
+  Busy_Control_Port.INTCTRL  = PORT_INT0LVL0_bm | PORT_INT0LVL1_bm; // Low-Level interrupt 0 for PORTD
+  Busy_Control_Port.INT0MASK = Busy_Control_Pin;
+  Busy_Control_Port.Busy_Control_PinCtrl = PORT_ISC_BOTHEDGES_gc | PORT_OPC_PULLUP_gc ;
+}
+
+void deInitReadMonitor()
+{
+  Busy_Control_Port.INTCTRL  = 0; // Low-Level interrupt 0 for PORTD
+  Busy_Control_Port.INT0MASK = Busy_Control_Pin;
+  Busy_Control_Port.Busy_Control_PinCtrl = PORT_ISC_INPUT_DISABLE_gc | PORT_OPC_PULLUP_gc ;
 }
 
 ISR( Busy_Control_IntVec )
 {
-	sendFree = false;
-	sendAnswerFree = false;
-	TCC2.LCNT = 20;
-//	LED_GRUEN_ON;
+  sendFree = false;
+  sendAnswerFree = false;
+  TCC2.LCNT = 20;
+  //LED_ROT_ON;
 }
 
 void initBusyCounter()
 {
-	TCC2.CTRLE = TC2_BYTEM_SPLITMODE_gc;
-	TCC2.CTRLA = TC2_CLKSEL_DIV256_gc;
-	TCC2.CTRLB = 0;
-	TCC2.INTCTRLA = TC2_LUNFINTLVL_LO_gc;
-	TCC2.LCNT = 128; // 128
-	TCC2.LPER = 42;
+  TCC2.CTRLE = TC2_BYTEM_SPLITMODE_gc;
+  TCC2.CTRLA = TC2_CLKSEL_DIV256_gc;
+  TCC2.CTRLB = 0;
+  TCC2.INTCTRLA = TC2_LUNFINTLVL_HI_gc;
+  TCC2.LCNT = 128; // 128
+  TCC2.LPER = 42;
 }
 
 ISR ( TCC2_LUNF_vect )
@@ -44,9 +52,9 @@ ISR ( TCC2_LUNF_vect )
   if(sendAnswerFree == true )
   {
       sendFree = true;
-//      LED_GRUEN_OFF;
+      //LED_ROT_OFF;
   }
   else
       sendAnswerFree = true;
-	TCC2.LCNT = 20;
+  TCC2.LCNT = 20;
 }
--- CmultiBusy.h
+++ CmultiBusy.h
@@ -3,13 +3,14 @@
  *
  * Created: 24.05.2018 13:34:11
  *  Author: chrak_2
- */ 
+ */
 
 
 #ifndef CMULTIBUSY_H_
 #define CMULTIBUSY_H_
 
 void initReadMonitor(void);
+void deInitReadMonitor(void);
 void initBusyCounter(void);
 
 #endif /* CMULTIBUSY_H_ */
