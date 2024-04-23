dcd6a542fbc2193859e4677ed775b2cd21725c73
chrak <kaufen@cmja.de>
2024-04-23 21:59:41 +0200

    Erstausgabe

---
+++ CmultiAddresses.h
@@ -0,0 +1,13 @@
+#ifndef CMULTIADDRESSES_H_INCLUDED
+#define CMULTIADDRESSES_H_INCLUDED
+
+// Ventilsteuerungen
+#define V1 "V1" // WC
+#define V2 "V2" // Bad 1. OG
+
+// Rollladensteuerungen
+#define R1 "R1"
+#define R2 "R2"
+#define R3 "R3"
+
+#endif // CMULTIADDRESSES_H_INCLUDED
