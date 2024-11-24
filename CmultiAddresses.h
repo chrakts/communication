835bc3735c7b05c7b3f203c266fbdb41aab22f97
chrak <kaufen@cmja.de>
2024-11-24 08:26:09 +0100

    neue Adressen

--- CmultiAddresses.h
+++ CmultiAddresses.h
@@ -6,14 +6,14 @@
 #define V2 "V2" // Bad 1. OG
 
 // Rollladensteuerungen
-#define R0 "R0"     // Arbeitszimmer
-#define R1 "R1"
+#define R0 "R0"     // Arbeitszimmer Fenster + Tür
+#define R1 "R1"     // Esszimmer Fenster + Tür + Markise
 #define R2 "R2"
 #define R3 "R3"
 
 // Tastersteuerung
-#define T0 "T0"  // 5x3 Tastaturmatrix
-#define T1 "T1"  //
-#define T2 "T2"  //
+#define T1 "T1"  // 5x3 Tastaturmatrix Arbeitszimmer
+#define T2 "T2"  // Funktaster für Esszimmer Fenster + Tür + Markise
+#define T3 "T3"  //
 
 #endif // CMULTIADDRESSES_H_INCLUDED
